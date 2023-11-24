124db8aed7911a7bd36903061a1d43f066c5f8f1
Tomas Resnik <101259872+Meizers@users.noreply.github.com>
2023-11-24 09:58:14 -0300

    Delete scrabber directory

--- scrabber/src/ar/edu/unlu/poo/scrabber/vista/Observador.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unlu.poo.scrabber.vista;
-
-public interface Observador {
-
-    void update();
-}
--- scrabber/src/ar/edu/unlu/poo/scrabber/vista/SujetoObservable.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.unlu.poo.scrabber.vista;
-
-public interface SujetoObservable {
-    void notificar();
-}
